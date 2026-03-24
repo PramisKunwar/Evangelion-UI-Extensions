07fd68ddd6d9f5868024bf9d46a07a3f4c59ddda
PramisKunwar <pramiskwr123@gmail.com>
2026-03-24 16:50:03 +0545

    Written content scripts

---
+++ content.js
@@ -0,0 +1,227 @@
+/*
+Grade 12 computer science
+
+
+Software development Life cycle
+
+Planning
+Analysis:  
+            Feasibility Study: 
+                                Economic
+                                Schedule
+                                Operational
+                                Technical
+                                Legal
+Design
+Development
+Testing
+Implement
+Maintenance
+
+Requirement Engineering- communication is one of the most important aspect
+It includes mainly 4 process:
+   a. Feasibility study
+   b. Requirement gathering
+   c. Software Requirement specification
+   d. Software Requirement validation
+
+Tools for requirement engineering:
+ a. Observation
+ b. Interview / Survey / Polls
+ c. Use cases
+ d. Workshop
+ e. Prototyping
+
+
+Waterfall model:
+It is a systematic and sequential approach to develop a software.
+
+Its processes are: 
+  Requirement
+  Design
+  Development
+  Testing
+  Implement
+  Maintenance
+
+Advantages:
+1. It is suitable for small projects
+2. It is less expensive
+3. Developer having basic understanding of the process and apply this model
+
+Disadvantages:
+1. It is not suitable for large projects
+2. It has no back track mechanism
+3. It lacks proper documentation
+
+
+Prototyping Model:
+It is an iterative process of system development where there is no clear idea of the requirements of the system.
+
+Its processes are:
+   Requirement gathering
+   Design
+   Prototype
+   Customer feedback
+   Refining the prototype
+   Final product
+
+Advantages:
+ 1. Errors can be detected quickly
+ 2. User feedback helps to improve the system
+ 3. Better understanding of the system 
+ 
+Disadvantages:
+ 1. The process becomes complex.
+ 2. The focus is mainly in testing and implementation.
+r
+Spiral model:
+It is a combination of both waterfall and prototype model.
+
+Its processes are:
+ 1. Planning
+ 2. Risk Analysis
+ 3. Design
+ 4. Development
+ 5. User evaluation
+
+Advantages: 
+ 1. It is good for large project.
+ 2. Good for providing regular updates.
+ 3. It emphasizes quality.
+ 4. It emphasized risk reducing techniques
+
+Disadvantages:
+ 1. It includes risk analysis so it is too lengthy for small projects.
+ 2. It is quite expensive. 
+
+Agile software development model
+It is based on iterative process and incremental development in which documenting is a key role.
+
+
+Planning
+Analysis --->> System analyst- communication, documentation, analysis
+Design
+
+Develop
+Testing----->> Software engineer- coding, problem solving, technical system knowledge
+Implementation 
+Deploy
+
+*/
+
+/*
+Maths:
+  Permutation and Combination
+  Binomial Theorem
+  Complex number
+  Sequence and series
+  Matrix
+
+  Circle
+  Parabola
+  Ellipse
+  Hyperbola
+
+  Vector
+
+Statistics: Correlation and Regression 
+Carl Pearson Coffecient 
+
+
+Regression equation of y on x and x on y.
+x = summation of X.Y + summation of X + summation of Y over Summation of X squared + summation of X whole squared
+
+Calculus:
+  Derivatives
+  Anti-derivatives
+  Differential Equation
+
+Computational Methods:
+  Gauss Elimination Method
+  Gauss Seidel Method
+  LPP by simplex method
+    By maximization
+    By minimization
+    pivot variable
+    slack variable
+    minimum
+    maximum
+
+  I have submitted my this project and the reviewer says like this:
+
+  what should I do now? Guide me
+
+  will my new hours be counted after I resubmit the improved one?
+
+*/
+
+
+/*
+
+  derivative of hyberbolic trigonometric functions
+  sinhx  ==>
+  coshx  ==>
+  tanhx ==> (x<1)  
+  cothx ==> (x>1)
+  sechx ==>
+  cosechx ==>
+
+
+    sin-1x ==> root
+    cos-1x ==> negative and same 
+    tan-1x ==> normal
+    cot-1x ==> negative and same
+    sec-1x ==> normal . root
+    cosec-1x ==> negative and same
+
+
+    sinh-1x ==> 1 over 
+    cosh-1x ==>
+    tanh-1x ==>
+    coth-1x ==>
+    sech-1x ==>
+    cosech-1x ==>
+
+
+
+      Application of derivative
+
+      1. L-Hospital's Rule
+      Let f(x) and g(x) be two differential function at x=a.
+      If f(a)=g(a)=0 then
+
+      If f(a)=g(a)= infinity then
+
+
+     2. Differentials
+
+     y = f(x), dx = ⨻x
+
+    ⨻y = f(x+⨻x) - f(x)
+
+    dy = f'(x). dx
+
+    actual change =  ⨻y
+
+    approximate change = dy 
+
+    error = actual change - approximate change over y times 100%
+
+  Tangent and Normal
+
+  slope of tangent to the curve = dy/dx
+
+  slope of normal to the curve = -dx/dy
+
+
+  equation of tangent at (x1,y1) = y-y1 = dy/dx (x-x1)
+
+  equation of normal at (x1,y1) = y-y1 = -dx/dy (x-x1)
+
+  for horizontal tangent 
+  dy/dx = 0
+
+  for vertical tangent 
+
+*/
